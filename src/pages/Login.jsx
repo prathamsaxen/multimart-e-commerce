6e87595f4fe58939069c5e84998374ec85c84e32
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-06-05 09:43:34 +0530

    Login Form Implemented!

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import "../index.css";
+
 
 function Login() {
   return (
-    <div className='Login'>
-      <h2>Login</h2>
+    <div className="Login">
+        <Form className="form-login">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group> */}
+          <Button variant="primary" type="submit" className=" m-auto d-block mt-5 w-100">
+            Login
+          </Button>
+        </Form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
