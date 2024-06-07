fb06c3a157053bbb3484560fe3161a4146949daf
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-06-07 10:55:00 +0530

    Fixed USer Cards

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,62 +1,99 @@
-import React,{useContext} from 'react'
-import AuthenticationContext from '../context/AuthenticationContext';
-import { Container,Row,Col } from 'react-bootstrap';
-import { serviceData } from '../Configuration/Home';
-import { useNavigate } from 'react-router-dom';
+import React, { useContext } from "react";
+import AuthenticationContext from "../context/AuthenticationContext";
+import { Container, Row, Col } from "react-bootstrap";
+// import { serviceData } from "../Configuration/Home";
+import { useNavigate } from "react-router-dom";
 function User() {
-    const {setLogin}=useContext(AuthenticationContext);
-    const navigate=useNavigate();
-    
-    const logoutUser=()=>{
-        localStorage.removeItem('token');
-        setLogin(false);
-        navigate("/");
-    }
-    
+  const { setLogin } = useContext(AuthenticationContext);
+  const navigate = useNavigate();
+
+  const logoutUser = () => {
+    localStorage.removeItem("token");
+    setLogin(false);
+    navigate("/");
+  };
+
   return (
     <div>
-      <section className='wrapper background'>
+      <section className="wrapper background">
         <Container>
           <Row>
-          <Col md={3} sm={5} xs={9} style={{backgroundColor:serviceData[0].bg}} className='feature' >
-                <div className='icon'>
+            <Col
+              md={3}
+              sm={5}
+              xs={9}
+              style={{ backgroundColor: "#fdefe6" }}
+              className="feature"
+              // style={{maxWidth:"1200px"}}
+            >
+              <div className="icon">
                 <ion-icon name="cart"></ion-icon>
-
-                </div>
-                <h3>{"Your Orders"}</h3>
-                <p>{serviceData[0].subtitle}</p>
-              </Col>
-              <Col md={3} sm={5} xs={9} style={{backgroundColor:"#ceebe9"}} className='feature' >
-                <div className='icon'>
+              </div>
+              <h3>{"Your Orders"}</h3>
+              <p>
+                {
+                  "You can view your past orders, track and return them, and repurchase items with ease."
+                }
+              </p>
+            </Col>
+            <Col
+              md={3}
+              sm={5}
+              xs={9}
+              style={{ backgroundColor: "#ceebe9" }}
+              className="feature"
+            >
+              <div className="icon">
                 <ion-icon name="log-in"></ion-icon>
-
-                </div>
-                <h3>{"Login & Security"}</h3>
-                <p>{serviceData[0].subtitle}</p>
-              </Col>
+              </div>
+              <h3>{"Login & Security"}</h3>
+              <p>
+                {
+                  "You can review and update your credentials and personal information."
+                }
+              </p>
+            </Col>
           </Row>
           <Row>
-          <Col md={3} sm={5} xs={9} style={{backgroundColor:"#e2f2b2"}} className='feature' >
-                <div className='icon'>
+            <Col
+              md={3}
+              sm={5}
+              xs={9}
+              style={{ backgroundColor: "#e2f2b2" }}
+              className="feature"
+            >
+              <div className="icon">
                 <ion-icon name="location-outline"></ion-icon>
-
-                </div>
-                <h3>{"Your Addresses"}</h3>
-                <p>{serviceData[0].subtitle}</p>
-              </Col>
-              <Col md={3} sm={5} xs={9} style={{backgroundColor:"#d6e5fb"}} className='feature' >
-                <div className='icon'>
+              </div>
+              <h3>{"Your Addresses"}</h3>
+              <p>
+                {
+                  "You can add or delete multiple addresses here for product delivery."
+                }
+              </p>
+            </Col>
+            <Col
+              md={3}
+              sm={5}
+              xs={9}
+              style={{ backgroundColor: "#d6e5fb" }}
+              className="feature"
+            >
+              <div className="icon">
                 <ion-icon name="call-outline"></ion-icon>
-
-                </div>
-                <h3>{"Contact Us"}</h3>
-                <p>{serviceData[0].subtitle}</p>
-              </Col>
+              </div>
+              <h3>{"Contact Us"}</h3>
+              <p>
+                {
+                  "For any queries or issues related to Craft Vision, you can contact us directly here."
+                }
+              </p>
+            </Col>
           </Row>
         </Container>
       </section>
     </div>
-  )
+  );
 }
 
-export default User
+export default User;
