e0cd61b2da61f0bdc93eea06c641d03ab35cab92
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-06-08 02:06:11 +0530

    Error 404 Done!

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import img from "../Images/404.png";
+import { useNavigate } from "react-router-dom";
 
 function Error404() {
+    const navigate=useNavigate();
   return (
-    <div className='Error404'>
-      <h2>Error404</h2>
+    <div className="Error404">
+      <img src={img} alt="Error in Loading" />
+      <div className="content404">
+        <h2>Error404</h2>
+        <p>Sorry, the page you are looking for cannot be found. Please check the URL or try navigating back to home page.</p>
+        <button onClick={()=>navigate("/")}>Go to HomePage</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Error404
+export default Error404;
