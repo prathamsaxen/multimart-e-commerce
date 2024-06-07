e3bdff1ebd3fa3de77935d103a1171c7988144bb
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-06-07 10:27:29 +0530

    Cards Added In User!

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,5 +1,7 @@
 import React,{useContext} from 'react'
 import AuthenticationContext from '../context/AuthenticationContext';
+import { Container,Row,Col } from 'react-bootstrap';
+import { serviceData } from '../Configuration/Home';
 import { useNavigate } from 'react-router-dom';
 function User() {
     const {setLogin}=useContext(AuthenticationContext);
@@ -13,8 +15,46 @@ function User() {
     
   return (
     <div>
-      <h2>About me!</h2>
-      <button onClick={logoutUser}>Logout</button>
+      <section className='wrapper background'>
+        <Container>
+          <Row>
+          <Col md={3} sm={5} xs={9} style={{backgroundColor:serviceData[0].bg}} className='feature' >
+                <div className='icon'>
+                <ion-icon name="cart"></ion-icon>
+
+                </div>
+                <h3>{"Your Orders"}</h3>
+                <p>{serviceData[0].subtitle}</p>
+              </Col>
+              <Col md={3} sm={5} xs={9} style={{backgroundColor:"#ceebe9"}} className='feature' >
+                <div className='icon'>
+                <ion-icon name="log-in"></ion-icon>
+
+                </div>
+                <h3>{"Login & Security"}</h3>
+                <p>{serviceData[0].subtitle}</p>
+              </Col>
+          </Row>
+          <Row>
+          <Col md={3} sm={5} xs={9} style={{backgroundColor:"#e2f2b2"}} className='feature' >
+                <div className='icon'>
+                <ion-icon name="location-outline"></ion-icon>
+
+                </div>
+                <h3>{"Your Addresses"}</h3>
+                <p>{serviceData[0].subtitle}</p>
+              </Col>
+              <Col md={3} sm={5} xs={9} style={{backgroundColor:"#d6e5fb"}} className='feature' >
+                <div className='icon'>
+                <ion-icon name="call-outline"></ion-icon>
+
+                </div>
+                <h3>{"Contact Us"}</h3>
+                <p>{serviceData[0].subtitle}</p>
+              </Col>
+          </Row>
+        </Container>
+      </section>
     </div>
   )
 }
