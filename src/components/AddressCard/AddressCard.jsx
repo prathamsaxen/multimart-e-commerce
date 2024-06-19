84ed5f330b991a039917dedcacfe4cdc0f207401
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-06-19 22:00:39 +0530

    add address card created!

--- src/components/AddressCard/AddressCard.jsx
+++ src/components/AddressCard/AddressCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./AddressCard.css";
+import plus from "../../Images/plus.png";
 
 function AddressCard() {
   return (
@@ -21,4 +22,13 @@ function AddressCard() {
   );
 }
 
+function AddAddressCard(){
+  return(
+  <div className="addaddressCard">
+    <img src={plus} alt="Error in Loading" />
+  </div>
+  );
+}
+
 export default AddressCard;
+export {AddAddressCard};
--- src/pages/Addresses.jsx
+++ src/pages/Addresses.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AddressCard from "../components/AddressCard/AddressCard";
+import { AddAddressCard } from "../components/AddressCard/AddressCard"; // Assuming it's a named export and a component
 
 function Addresses() {
   return (
     <div className="Addresses">
-      <h2>Adresses</h2>
-      <AddressCard/>
+      <h2>Addresses</h2>
+      <AddressCard />
+      <AddAddressCard /> {/* Use it as a component */}
     </div>
   );
 }
