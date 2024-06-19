e55ed6fee1ac770721bb9becbd23140605c07cbe
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-06-20 00:01:26 +0530

    Contact FOrm Implemented!

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,66 +1,44 @@
 import React from "react";
-
+import "../index.css";
 function Contact() {
   return (
-    <div className="Contact container">
-      <h2 className="py-4">Contact Us</h2>
-      <div className="contactForm">
-        <form class="row g-3 needs-validation" novalidate>
-          <div class="col-md-12">
-            <label for="validationCustom01" class="form-label">
-              Name
-            </label>
-            <input
-              type="text"
-              class="form-control"
-              id="validationCustom01"
-              required
-            />
-            <div class="valid-feedback">Looks good!</div>
-          </div>
-          <div class="col-md-12">
-            <label for="validationCustomUsername" class="form-label">
-              Phone Number
-            </label>
-            <div class="input-group has-validation">
-              <span class="input-group-text" id="inputGroupPrepend">
-                +91
-              </span>
-              <input
-                type="text"
-                class="form-control"
-                id="validationCustomUsername"
-                aria-describedby="inputGroupPrepend"
-                required
-              />
-              <div class="invalid-feedback">Please choose a mobile Number</div>
+    <div className="contact-page">
+      <div className="contact-us-page-section">
+        <div className="contact-us-form">
+          <form action="">
+          <h2>Contact Us</h2>
+            <div className="contact-us-form-group">
+              <div className="label-group">
+                <label for="name">Name:</label>
+                <input type="text" id="name" />
+              </div>
+              <div className="label-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" />
+              </div>
             </div>
-          </div>
-          <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              class="form-control"
-              id="exampleFormControlInput1"
-              placeholder="name@gmail.com"
-            />
-          </div>
-          <div class="form-floating ">
-            <textarea
-              class="form-control "
-              placeholder="Leave a comment here"
-              id="floatingTextarea2"
-            ></textarea>
-            <label for="floatingTextarea2">Message</label>
-          </div>
-          <div class="col-12">
-            <button class="detailsFormButton contactButton" type="submit">
-              Contact Us
-            </button>
-          </div>
-        </form>
+            <div className="label-group">
+              <label for="subject">Subject:</label>
+              <input type="text" id="subject" />
+            </div>
+            <div className="label-group">
+              <label for="message">Message:</label>
+              <textarea name="" id="message"></textarea>
+            </div>
+            <input type="submit" value={"Send"}/>
+          </form>
+        </div>
+        <div className="contact-us-map">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3526.0935956382436!2d78.05259667700845!3d27.8991009760733!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3974a5c176570e5f%3A0xaf0a8ca698aad504!2sFORELSKET%20SOFTWARES%20PVT%20LTD!5e0!3m2!1sen!2sin!4v1718818754211!5m2!1sen!2sin"
+            // width="600"
+            // height="450"
+            style={{ border: "none" }}
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
       </div>
     </div>
   );
