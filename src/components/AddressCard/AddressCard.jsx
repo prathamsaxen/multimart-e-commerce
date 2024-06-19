c9a6232219485f4f31dae51da94d44a681a3ce0d
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-06-19 21:33:47 +0530

    Fixed! Address Card!

--- src/components/AddressCard/AddressCard.jsx
+++ src/components/AddressCard/AddressCard.jsx
@@ -3,17 +3,21 @@ import "./AddressCard.css";
 
 function AddressCard() {
   return (
-      <div className="addressCard">
-        <h2>Sujal Varshney</h2>
-        <p>1/305 Naurangabad Chhawani, Aligarh naurangabad</p>
-        <p>ALIGARH, UTTAR PRADESH 202001 India</p>
-        <p>Phone number: 790635963</p>
-        <div className="addressButton">
-            <button>Edit</button>
-            <button>Delete</button>
-            <button>Set as Default</button>
-        </div>
+    <div className="addressCard">
+      <h2>Sujal Varshney</h2>
+      <p>1/305</p>
+      <p> Naurangabad Chhawani, Aligarh naurangabad</p>
+      <p>ALIGARH, UTTAR PRADESH 202001</p>
+      <p> India</p>
+      <p>
+        <b>Phone Number:</b> 790635963
+      </p>
+      <div className="addressButton">
+        <button>Edit</button>
+        <button>Delete</button>
+        <button>Set as Default</button>
       </div>
+    </div>
   );
 }
 
