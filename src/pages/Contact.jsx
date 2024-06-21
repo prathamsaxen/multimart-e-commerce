b9b87b32c4b103ffbf4be055813516396e5d42ba
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-06-22 01:08:05 +0530

    COntact Pge

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -113,9 +113,9 @@ function Contact() {
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3526.0935956382436!2d78.05259667700845!3d27.8991009760733!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3974a5c176570e5f%3A0xaf0a8ca698aad504!2sFORELSKET%20SOFTWARES%20PVT%20LTD!5e0!3m2!1sen!2sin!4v1718818754211!5m2!1sen!2sin"
             style={{ border: "none" }}
-            allowfullscreen=""
+            allowfullScreen=""
             loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
+            referrerPolicy="no-referrer-when-downgrade"
             title="Our Location"
           ></iframe>
         </div>
