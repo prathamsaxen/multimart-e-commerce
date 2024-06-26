0a63c9c56900c50baa8145bc2a77d1457df2ea35
Sujal Varshney <sujalvarshney1692@gmail.com>
2024-06-26 13:13:27 +0530

    Added Checkout OPage

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,77 +1,84 @@
 import React from "react";
 import "../index.css";
 
+
 function Checkout() {
   return (
-    <div className="container checkout">
-      <div className="left">
-        <div className="upper">
-          <div className="checkoutContact">
-            <p className="firstWidth">Contact</p>
-            <p className="secondWidth">forelsketsteam@gmail.com</p>
-            <button>Change</button>
+    <div className="checkout container">
+      <div className="checkout-payment">
+        <p className="fw-bold">SELECT PAYMENT METHOD</p>
+        <div className="payment-modes">
+          <div className="mode-of-payment">
+            <ion-icon name="card-outline"></ion-icon>
+            <div>Online Payment</div>
           </div>
-          <div className="checkoutShip">
-            <p className="firstWidth">Ship to</p>
-            <p>1/305 Naurangabad Chhawani, naurangabad, ALIGARH</p>
-            <button>Change</button>
+          {/* <div className="mode-of-payment">Bank Transfer</div> */}
+          <div className="mode-of-payment">
+            <ion-icon name="logo-paypal"></ion-icon>
+            <div>Cash On Delivery</div>
           </div>
         </div>
-        <div className="lower">
-        <h3>Payment Method</h3>
-            <div>
-              <div class="form-check">
-                <input
-                  class="form-check-input"
-                  type="radio"
-                  name="flexRadioDefault"
-                  id="flexRadioDefault1"
-                />
-                <label class="form-check-label" for="flexRadioDefault1">
-                  Cash On Delivery
-                </label>
-              </div>
-              <div class="form-check">
-                <input
-                  class="form-check-input"
-                  type="radio"
-                  name="flexRadioDefault"
-                  id="flexRadioDefault1"
-                />
-                <label class="form-check-label" for="flexRadioDefault1">
-                  Online
-                </label>
-              </div>
+        <div className="checkout-form">
+          <form class="row g-3">
+            <div class="col-12">
+              <label for="inputName" class="form-label fw-bold">
+                NAME ON CARD
+              </label>
+              <input type="text" class="form-control" id="inputName" />
             </div>
-        </div>
-        <div className="returnPayButton">
-            <button>Return to Cart</button>
-            <button className="paymentButton">Continue to payment</button>
+            <div class="col-12">
+              <label for="inputCard" class="form-label fw-bold">
+                CARD NUMBER
+              </label>
+              <input
+                type="text"
+                class="form-control card-input"
+                id="inputCard"
+                placeholder="Enter your card number"
+              />
+            </div>
+            <div class="col-4">
+              <label for="inputMonth" class="form-label fw-bold">
+                Month
+              </label>
+              <input type="text" class="form-control" id="inputMonth" />
+            </div>
+            <div class="col-4">
+              <label for="inputYear" class="form-label fw-bold">
+                Year
+              </label>
+              <input type="Number" class="form-control" id="inputYear" />
+            </div>
+            <div class="col-4">
+              <label for="inputCVV" class="form-label fw-bold">
+                CVV
+              </label>
+              <input type="Number" class="form-control" id="inputCVV" />
+            </div>
+          </form>
         </div>
       </div>
-      <div className="right">
-        <div className="cartProduct">
-            <img src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-madebymath-90946.jpg&fm=jpg"></img>
-            <p>Brain Blend</p>
-            <p>852</p>
+      <div className="checkout-details">
+        <div className="checkout-product">
+          <p className="fw-bold">ORDER SUMMARY</p>
+          <div className="product">
+            <p>Samsung FE 23</p>
+            <p>129999</p>
+          </div>
+          <p className="fs-6">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus, itaque?
+          </p>
         </div>
-        <div className="pt-5">
-            <div className="payment">
-                <p>Subtotal</p>
-                <p>464.00</p>
-            </div>
-            <div className="payment">
-                <p>Shipping</p>
-                <p>5.00</p>
-            </div>
-            <div className="payment">
-                <p>Estimated taxes</p>
-                <p>28.14</p>
-            </div>
-            <div className="totalPayment">
-                <p>Total</p>
-                <p>497.14</p>
-            </div>
+        <div className="checkout-date text-center">
+          <p className="fw-bold">WEDNESDAY</p>
+          <div className="d-flex justify-content-center fw-bold">
+            <p>January 18, 2017 | </p>
+            <p className="px-1">11:00 AM</p>
+          </div>
+        </div>
+        <div>
+          <button  className="payment-button">PURCHASE NOW</button>
         </div>
       </div>
     </div>
