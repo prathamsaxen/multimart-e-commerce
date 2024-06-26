d877c53465fd841d7123b48927f113d8a0674127
Sujal Varshney <sujalvarshney1692@gmail.com>
2024-06-26 19:08:20 +0530

    resolve error

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -81,7 +81,7 @@ const Cart = () => {
                 <h4>Total Price :</h4>
                 <h3>${totalPrice}.00</h3>
               </div>
-              <NavLink to={login?"/me/checkout":"/login?callbackurl=cart"} className="checkout-btn">Checkout</NavLink>
+              <NavLink to={login?"/me/checkout":"/login?callbackurl=cart"} disabled={true} className="checkout-btn">Checkout</NavLink>
             </div>
           </Col>
         </Row>
