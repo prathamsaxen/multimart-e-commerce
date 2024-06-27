768de48fe1a5f04ca03684842e4addb3a235d5ce
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-06-27 14:51:53 +0530

    Fixed Cart Number Bug!

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import {  useState,useContext } from "react";
+import { useState, useContext } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 // import { useSelector } from "react-redux";
@@ -16,7 +16,7 @@ const NavBar = () => {
       setIsFixed(false);
     }
   }
-  const {login}=useContext(AuthenticationContext);
+  const { login } = useContext(AuthenticationContext);
   window.addEventListener("scroll", scrollHandler);
   // useEffect(()=> {
   //   if(CartItem.length ===0) {
@@ -52,11 +52,10 @@ const NavBar = () => {
             </svg>
             <Link
               aria-label="Go to Cart Page"
-              to={login?"/cart":"/login?callbackurl=cart"}
+              to={login ? "/cart" : "/login?callbackurl=cart"}
               className="cart"
               data-num={login.cartItems}
-
-            > 
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
@@ -113,12 +112,11 @@ const NavBar = () => {
               </Link>
             </Nav.Item> */}
             <Nav.Item className="expanded-cart">
-              
               <Link
                 aria-label="Go to Cart Page"
-                to={login?"/cart":"/login?callbackurl=cart"}
+                to={login ? "/cart" : "/login?callbackurl=cart"}
                 className="cart"
-                data-num={login.cartItems}
+                data-num={login.cartItems || 0}
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -134,21 +132,21 @@ const NavBar = () => {
               <Link
                 aria-label="Go to Cart Page"
                 className="navbar-link"
-                to={login?"/me":"login"}
+                to={login ? "/me" : "login"}
                 onClick={() => setExpand(false)}
               >
                 <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="black"
-                className="nav-icon"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
-                  clipRule="evenodd"
-                />
-              </svg>
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="black"
+                  className="nav-icon"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+                    clipRule="evenodd"
+                  />
+                </svg>
               </Link>
             </Nav.Item>
           </Nav>
