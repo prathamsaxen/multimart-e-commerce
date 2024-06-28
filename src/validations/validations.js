da55d7dd45bcfdd396f4a98f94ee78988929e52e
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-06-28 12:36:31 +0530

    City Validation FIxed!

--- src/validations/validations.js
+++ src/validations/validations.js
@@ -9,6 +9,7 @@ const flatNumberRegex = /^[A-Za-z0-9\s\-\/]+$/;
 const areaRegex = /^[A-Za-z0-9\s\-,]+$/;
 const landmarkRegex = /^[A-Za-z0-9\s\-,]+$/;
 const stateRegex = /^[A-Za-z\s]+$/;
+const cityRegex = /^[A-Za-z\s]+$/;
 
 const addressValidation = (inputAddress) => {
   if (!inputAddress.name) {
@@ -27,28 +28,39 @@ const addressValidation = (inputAddress) => {
     toast.error("Country is required");
     return false;
   } else if (!countryRegex.test(inputAddress.country)) {
-    toast.error("Country can only contain alphabetic characters, spaces, and hyphens");
+    toast.error(
+      "Country can only contain alphabetic characters, spaces, and hyphens"
+    );
     return false;
   } else if (!inputAddress.flat) {
     toast.error("Flat/House No. is required");
     return false;
   } else if (!flatNumberRegex.test(inputAddress.flat)) {
-    toast.error("Flat/House No. can only contain alphanumeric characters, spaces, hyphens, and slashes");
+    toast.error(
+      "Flat/House No. can only contain alphanumeric characters, spaces, hyphens, and slashes"
+    );
     return false;
   } else if (!inputAddress.area) {
     toast.error("Area is required");
     return false;
   } else if (!areaRegex.test(inputAddress.area)) {
-    toast.error("Area can only contain alphanumeric characters, spaces, hyphens, and commas");
+    toast.error(
+      "Area can only contain alphanumeric characters, spaces, hyphens, and commas"
+    );
     return false;
   } else if (!inputAddress.landmark) {
     toast.error("Landmark is required");
     return false;
   } else if (!landmarkRegex.test(inputAddress.landmark)) {
-    toast.error("Landmark can only contain alphanumeric characters, spaces, hyphens, and commas");
+    toast.error(
+      "Landmark can only contain alphanumeric characters, spaces, hyphens, and commas"
+    );
     return false;
   } else if (!inputAddress.city) {
     toast.error("City is required");
+    return false;
+  } else if (!cityRegex.test(inputAddress.city)) {
+    toast.error("City can only contain alphabetic characters and spaces");
     return false;
   } else if (!inputAddress.state) {
     toast.error("State is required");
