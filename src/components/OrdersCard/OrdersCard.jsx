f3e37c7cccde1f5273cad6b692147887a4d15eef
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-06-28 11:54:53 +0530

    ORders Card Template Created!

---
+++ src/components/OrdersCard/OrdersCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./OrdersCard.css";
+function OrdersCard() {
+  return <div className="OrdersCard"></div>;
+}
+
+export default OrdersCard;
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,56 +1,12 @@
 import React from "react";
 import "../styles/Orders.css";
+import OrdersCard from "../components/OrdersCard/OrdersCard";
 
 function Orders() {
   return (
     <div className="Orders container">
       <h2>Order</h2>
-      <div className="allOrderCards">
-        <div className="orderCard">
-          <div className="upper">
-            <div className="left">
-              <div className="first">
-                <div>ORDER PLACED</div>
-                <div>12 june 2024</div>
-              </div>
-              <div className="second">
-                <div>TOTAL PRICE</div>
-                <div>
-                  <p>897.00</p>
-                </div>
-              </div>
-              <div className="third">
-                <div>SHIP TO</div>
-                <div>Sujal Varshney</div>
-              </div>
-            </div>
-            <div className="right">
-              <div>
-                <div>ORDER ID</div>
-                <div className="order">
-                  <p>View order details</p>
-                  <p>Invoice</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="bottom">
-            <div className="left">
-              <p>Arriving Saturday</p>
-              <p>On the way</p>
-              <div className="orderImage">
-                <img src="https://kraftvision.com/cdn/shop/files/0be9d5e102a9c3c43b238433014d6700.jpg?v=1696145148" alt="Error in loading!"></img>
-                <p>name of the parcel</p>
-              </div>
-            </div>
-            <div className="right">
-              <button className="package">Track Package</button>
-              <button>Cancel this delivery</button>
-              <button>Leave seller feedback</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <OrdersCard />
     </div>
   );
 }
